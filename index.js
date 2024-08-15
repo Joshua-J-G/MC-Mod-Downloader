e23ba33c1a1a78a4a8ade6c9887cf30e172db5ac
Joshua Gessner <z5593524@ad.unsw.edu.au>
2024-08-16 02:57:09 +1000

    changed from resolution to scale factor

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ const MathConsts = require("./src/MathConsts.js")
 
 const createWindow = () => {
     const primaryDisplay = screen.getPrimaryDisplay()
-    const { width, height } = primaryDisplay.size
-    // Console information about the user screen and the set resolution
-    console.log(` 
-      Screen Width: ${width}
-      Screen Height ${height}
-      ________________________
-      Calculated Width: ${Math.floor(parseFloat(width) / MathConsts.PI)}
-      Calculated Height: ${Math.floor(parseFloat(height) / MathConsts.RydbergUnitOfEnergy)}
-      `);
+  
+  
 
     const win = new BrowserWindow({
         // Ha Ha Funny Math Numbers
-        width: Math.floor(parseFloat(width) / MathConsts.PI),
-        height: Math.floor(parseFloat(height) / MathConsts.RydbergUnitOfEnergy)
+        width: 800 / primaryDisplay.scaleFactor,
+        height: 600 / primaryDisplay.scaleFactor
     });
 
     win.loadFile('index.html')
