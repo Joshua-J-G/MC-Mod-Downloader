66b2897ea93360cb61b5812e216524b249bc4f20
Joshua Gessner <z5593524@ad.unsw.edu.au>
2024-08-16 02:59:10 +1000

    reveted change

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const MathConsts = require("./src/MathConsts.js")
 
 const createWindow = () => {
     const primaryDisplay = screen.getPrimaryDisplay()
-  
-  
+    const { width, height } = primaryDisplay.size
+    // Console information about the user screen and the set resolution
+    console.log(` 
+      Screen Width: ${width}
+      Screen Height ${height}
+      ________________________
+      Calculated Width: ${Math.floor(parseFloat(width) / MathConsts.PI)}
+      Calculated Height: ${Math.floor(parseFloat(height) / MathConsts.RydbergUnitOfEnergy)}
+      `);
 
     const win = new BrowserWindow({
         // Ha Ha Funny Math Numbers
-        width: 800 / primaryDisplay.scaleFactor,
-        height: 600 / primaryDisplay.scaleFactor
+        width: Math.floor(parseFloat(width) / MathConsts.PI),
+        height: Math.floor(parseFloat(height) / MathConsts.RydbergUnitOfEnergy)
     });
 
     win.loadFile('index.html')
